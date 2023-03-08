4339ae5105da61c3b090fe601a9cdbd258c84496
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-08 08:46:44 +0530

    [Ganesh] Add:Logical programming file is added

---
+++ LogicalPrograming/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LogicalPrograming
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to logical programming");
+        }
+    }
+}
